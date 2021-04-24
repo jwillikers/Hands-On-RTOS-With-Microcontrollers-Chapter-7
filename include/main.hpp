add0478530ce2f38ff1c1c4b8ec7418df63d646b
Jordan Williams <jordan@jwillikers.com>
2021-04-24 13:41:01 -0500

    Remove extern "C" for C++ header main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -1,30 +1,7 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.h
- * @brief          : Header for main.c file.
- *                   This file contains the common defines of the application.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under Ultimate Liberty license
- * SLA0044, the "License"; You may not use this file except in compliance with
- * the License. You may obtain a copy of the License at:
- *                             www.st.com/SLA0044
- *
- ******************************************************************************
- */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #pragma once
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 /* Includes ------------------------------------------------------------------*/
 #include <stm32f7xx_hal.h>
@@ -113,9 +90,3 @@ static constexpr uint32_t LD1_Pin{GPIO_PIN_0};
 #define LD1_GPIO_Port GPIOB
 
 /* USER CODE END Private defines */
-
-#ifdef __cplusplus
-}
-#endif
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
